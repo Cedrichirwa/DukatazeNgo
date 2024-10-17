e7481ff5b52f9c5b2e03b0509b32f7c3fe9f73b3
Cedrichirwa <101490856+Cedrichirwa@users.noreply.github.com>
2024-10-17 21:11:33 +0200

    Update Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -27,16 +27,19 @@ const Contact = () => {
   return (
     <>
       {/* First Section - Contact Header */}
-      <div className="relative flex md:items-center md:justify-center mb-6 md:h-screen bg-center bg-cover">
-        <Image src={contact} alt="contact" layout="fill" objectFit="cover" />
-
-        <div className="absolute inset-0 bg-black/70 z-[2]" />
-        <div className="p-5 text-white z-[3] absolute flex flex-col items-center justify-center w-full h-full">
-          <h2 className="text-4xl md:block hidden sm:text-6xl font-bold">
-            Contact Us
+      <div className="relative flex md:items-center md:justify-center mb-6 md:h-screen sm:bg-fixed bg-center bg-cover coll">
+        <Image
+          src={contact}
+          alt="contact"
+          className="w-full h-full md:hidden block"
+        />
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-black/70 z-[2]" />
+        <div className="p-5 text-white z-[2] absolute flex flex-col items-center justify-center w-full h-full">
+          <h2 className="text-4xl md:block hidden sm:text-6xl font-bold mx-auto text-center">
+          Contact Us
           </h2>
-          <p className="p-4 text-xl sm:text-3xl text-yellow-600">
-            Reach out to us with any inquiry
+          <p className="p-4 text-xl sm:text-3xl text-yellow-600 ">
+          Reach out to us with any inquiry
           </p>
         </div>
       </div>
